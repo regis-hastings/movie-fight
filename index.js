e0c0df9501417f1d2e1269192c576313e3205351
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-07 16:30:33 -0700

    add fetch functionality

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: 'f1b0b3d1',
+			i: 'tt0848228'
+		}
+	});
+
+	console.log(response.data);
+};
+
+fetchData();
