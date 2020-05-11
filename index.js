38eeac4924847d2c3f1c7f067f5bd5b53e24bdf4
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-10 18:36:23 -0700

    add display functionality

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ const fetchData = async (searchTerm) => {
 		}
 	});
 
-	console.log(response.data);
+	return response.data.Search;
 };
 
 const input = document.querySelector('input');
 
-const onInput = (event) => {
-	fetchData(event.target.value);
+const onInput = async (event) => {
+	const movies = await fetchData(event.target.value);
+	for (let movie of movies) {
+		const div = document.createElement('div');
+
+		div.innerHTML = `
+			<img src="${movie.Poster}"/>
+		`;
+	}
 };
 
 input.addEventListener('input', debounce(onInput, 500));
