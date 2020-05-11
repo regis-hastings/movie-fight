12b78d6bf4513b93542f1928dcfbbbeaed5614b0
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-10 19:46:08 -0700

    add dropdown widget functionality

--- index.js
+++ index.js
@@ -6,22 +6,42 @@ const fetchData = async (searchTerm) => {
 		}
 	});
 
+	if (response.data.Error) {
+		return [];
+	}
+
 	return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+	<label><b>Search for a Movie</b></label>
+	<input class="input" />
+	<div class="dropdown">
+		<div class="dropdown-menu">
+			<div class="dropdown-content results"></div>
+		</div>
+	</div>
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async (event) => {
 	const movies = await fetchData(event.target.value);
-	for (let movie of movies) {
-		const div = document.createElement('div');
 
-		div.innerHTML = `
+	dropdown.classList.add('is-active');
+	for (let movie of movies) {
+		const option = document.createElement('a');
+
+		option.classList.add('dropdown-item');
+		option.innerHTML = `
 			<img src="${movie.Poster}"/>
-			<h1>${movie.Title}</h1>
+			${movie.Title}
 		`;
 
-		document.querySelector('#target').append(div);
+		resultsWrapper.append(option);
 	}
 };
 
