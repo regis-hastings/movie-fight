d214009ab5bb052d4dc257da5909d5b4e71bc43d
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-11 20:56:11 -0700

    add movieTemplate function

--- index.js
+++ index.js
@@ -73,5 +73,24 @@ const onMovieSelect = async (movie) => {
 		}
 	});
 
-	console.log(response.data);
+	document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+	return `
+		<article class="media">
+			<figure class="media-left">
+				<p class="image">
+					<img src="${movieDetail.Poster}" />
+				</p>
+			</figure>
+			<div class="media-content">
+				<div class="content">
+					<h1>${movieDetail.Title}</h1>
+					<h4>${movieDetail.Title}</h4>
+					<p>${movieDetail.Plot}</p>
+				</div>
+			</div>
+		</article>
+	`;
 };
