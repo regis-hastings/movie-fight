9ff0301268ab3acf4218178633c9828155b800e8
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-11 20:32:46 -0700

    handle movie selection

--- index.js
+++ index.js
@@ -48,6 +48,10 @@ const onInput = async (event) => {
 			${movie.Title}
 		`;
 
+		option.addEventListener('click', () => {
+			dropdown.classList.remove('is-active');
+			input.value = movie.Title;
+		});
 		resultsWrapper.append(option);
 	}
 };
