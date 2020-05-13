50fac5b79c66dedcbadff9ef64a248d1401284be
regis-hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-13 12:27:22 -0700

    add extract renderOption function from autocomplete

--- index.js
+++ index.js
@@ -14,7 +14,14 @@ const fetchData = async (searchTerm) => {
 };
 
 createAutoComplete({
-	root: document.querySelector('.autocomplete')
+	root: document.querySelector('.autocomplete'),
+	renderOption(movie) {
+		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+		return `
+		<img src="${imgSrc}"/>
+		${movie.Title}(${movie.Year})
+	`;
+	}
 });
 
 const onMovieSelect = async (movie) => {
