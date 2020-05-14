0ca001f923070deaf0293ff4b82980249083f8d5
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-14 15:37:27 -0700

    extract statistics for movieTemplate

--- index.js
+++ index.js
@@ -73,6 +73,27 @@ const runComparison = () => {
 }
 
 const movieTemplate = (movieDetail) => {
+	const dollars = parseInt(
+		movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, '')
+	);
+	const metascore = parseInt(movieDetail.Metascore);
+	const imdbRating = parseFloat(movieDetail.imdbRating);
+	const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''));
+
+	let count = 0;
+
+	const awards = movieDetail.Awards.split(' ').reduce((prev, word) => {
+		const value = parseInt(word);
+
+		if(isNaN(value)) {
+			return prev;
+		} else {
+			return prev + value;
+		}
+	}, 0);
+	
+	console.log(awards);
+
 	return `
 		<article class="media">
 			<figure class="media-left">
