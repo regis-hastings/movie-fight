206f5e36c59325659d7f3c4696ce09f267c3cd87
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-14 14:35:59 -0700

    add runComparison function

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ createAutoComplete({
 	root: document.querySelector('#left-autocomplete'),
 	onOptionSelect(movie) {
 		document.querySelector('.tutorial').classList.add('is-hidden');
-		onMovieSelect(movie, document.querySelector('#left-summary'));
+		onMovieSelect(movie, document.querySelector('#left-summary'), 'left');
 	}
 });
 
@@ -39,11 +39,14 @@ createAutoComplete({
 	root: document.querySelector('#right-autocomplete'),
 	onOptionSelect(movie) {
 		document.querySelector('.tutorial').classList.add('is-hidden');
-		onMovieSelect(movie, document.querySelector('#right-summary'));
+		onMovieSelect(movie, document.querySelector('#right-summary'), 'right');
 	},
 });
 
-const onMovieSelect = async (movie, summaryElement) => {
+let leftMovie;
+let rightMovie;
+
+const onMovieSelect = async (movie, summaryElement, side) => {
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params: {
 			apikey: 'f1b0b3d1',
@@ -52,7 +55,21 @@ const onMovieSelect = async (movie, summaryElement) => {
 	});
 
 	summaryElement.innerHTML = movieTemplate(response.data);
+
+	if (side === 'left') {
+		leftMovie = response.data;
+	} else {
+		rightMovie = response.data;
+	}
+
+	if (leftMovie && rightMovie) {
+		runComparison();
+	}
 };
+
+const runComparison = () => {
+	console.log('Time for comparison')
+}
 
 const movieTemplate = (movieDetail) => {
 	return `
